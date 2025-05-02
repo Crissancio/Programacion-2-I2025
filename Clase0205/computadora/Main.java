41a73aa7a626bea394810083d4ca9df28c8ddeca
Cristhian Alvarez <64036218+Crissancio@users.noreply.github.com>
2025-05-02 17:20:36 +0000

    commit main

--- Clase0205/computadora/Main.java
+++ Clase0205/computadora/Main.java
@@ -1,5 +1,16 @@
 package Clase0205.computadora;
 
 public class Main{
+    public static void main(String[] args) {
+        
+        CPU cpu = new CPU(2.5F, 4, 2.4F, "LGA");
+        RAM ram = new RAM(16, 2, 4);
+        Memoria almacenamiento = new Memoria(512, 
+        "Kingtone", "SSD");
+        TarjetaMadre tarjetaMadre = new TarjetaMadre(8, "LGA", 5, 4, 128);
+        FuenteDePoder fuenteDePoder = new FuenteDePoder("Corsair", 550, "80 Plus");
 
+        PC computadora = new PC(cpu, ram, almacenamiento, tarjetaMadre, fuenteDePoder);
+        computadora.mostrar();
+    }
 }
