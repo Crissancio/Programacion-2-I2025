5de9b6cd8505fbf2c992b9705d6eb8109cdc467d
Crissancio <soyelpapu1509@gmail.com>
2025-06-06 11:37:53 -0400

    pesistencia persona python

--- Clase0606/Persona.py
+++ Clase0606/Persona.py
@@ -1,38 +1,56 @@
 import os
+import json
 
+rutaBase = "F:/program/aux_121/Programacion-2-I2025/Clase0606/personaPython"
 class Persona:
     def __init__(self, id, nom, edad):
         self.id = id
         self.nom = nom
         self.edad = edad
-        self.archivo = f"F:/program/aux_121/Programacion-2-I2025/Clase0606/personaPython/persona{self.id}.json"
+        self.archivo = f"{rutaBase}/persona{self.id}.json"
 
-    @classmethod
-    def desde_archivo(cls, id):
-        ruta = f"F:/program/aux_121/Programacion-2-I2025/Clase0606/personaPython/persona{id}.json"
-        with open(ruta, "r") as f:
-            datos = f.read().splitlines()
-            return cls(int(datos[0]), datos[1], int(datos[2]))
+    def cargarDatos(self, rutaArchivo):
+        self.archivo = rutaArchivo
+        if os.path.isfile(self.archivo):
+            with open(self.archivo, 'r') as archivo:
+                datos = json.load(archivo)
+                self.id = datos.get("id")
+                self.nom = datos.get("nom")
+                self.edad = datos.get("edad")
+                print(f"Datos cargados: persona{self.id}.json")
+        else:
+            print(f"El archivo persona{self.id}.json no existe.")
+        
+    def guardarDatos(self):
+        os.makedirs(rutaBase, exist_ok=True)
+        if not os.path.exists(self.archivo):
+            with open(self.archivo, 'w') as archivo:
+                datos = {
+                    "id": self.id,
+                    "nom": self.nom,
+                    "edad": self.edad
+                }
+                
+                with open(self.archivo, 'w') as archivo:
+                    json.dump(datos, archivo, indent=4)
+            print(f"Datos guardados en: persona{self.id}.son")
 
-    def guardar(self):
-        os.makedirs(os.path.dirname(self.archivo), exist_ok=True)
-        with open(self.archivo, "w") as f:
-            f.write(f"{self.id}\n{self.nom}\n{self.edad}\n")
+        
+    def actualizarDatos(self):
+        with open(self.archivo, 'r') as archivo:
+            datos = json.load(archivo)
+            datos["nom"] = self.nom
+            datos["edad"] = self.edad
+            with open(self.archivo, 'w') as archivo:
+                json.dump(datos, archivo, indent=4)
 
-    def actualizar(self, nuevo_nom, nueva_edad):
-        self.nom = nuevo_nom
-        self.edad = nueva_edad
-        self.guardar()
-
-    def eliminar(self):
-        if os.path.exists(self.archivo):
+    def eliminarDatos(self):
+        if os.path.isfile(self.archivo):
             os.remove(self.archivo)
-
+            print(f"Archivo persona{self.id}.json eliminado.")
+        else:
+            print(f"El archivo persona{self.id}.json no existe.")
+    
     def __str__(self):
         return f"ID: {self.id}, Nombre: {self.nom}, Edad: {self.edad}"
-
-
-p = Persona.desde_archivo(1)
-# p.actualizar("Tadeo", 52)
-
-p.eliminar()
+    
